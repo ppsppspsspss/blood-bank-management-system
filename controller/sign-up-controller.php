f03aa2c72fab029285fa009c0fc2165c93873cc4
This is Ratul <newidratul@gmail.com>
2023-12-29 23:13:06 +0600

    sign-up & controller updated

--- controller/sign-up-controller.php
+++ controller/sign-up-controller.php
@@ -1,14 +1,13 @@
 <?php
-
-require("validation.php");
 require("../model/user-info-model.php");
 
-if (isset($_POST['submit_button'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
     $dob = $_POST['dob'];
     $gender = $_POST['gender'];
     $country = $_POST['country'];
@@ -16,27 +15,132 @@ if (isset($_POST['submit_button'])) {
     $bloodGroup = $_POST['bloodGroup'];
     $role = "Manager";
 
-    if(
-        isValidName($firstName) &&
-        isValidName($lastName) &&
-        isValidEmail($email) &&
-        isValidPassword($password) &&
-        isValidPhone($phone, $country) &&
-        isValidBloodGroup($bloodGroup)
-    ){
-        
+    if (!isValidName($firstName)) {
+        header('location: ../view/sign-up.php?err=firstNameInvalid');
+        exit();
     }
-    else{ 
-        header('location: ../view/signup.php?err=invalid');
+    if (!isValidName($lastName)) {
+        header('location: ../view/sign-up.php?err=lastNameInvalid');
+        exit();
+    }
+    if (!isValidEmail($email)) {
+        header('location: ../view/sign-up.php?err=emailInvalid');
+        exit();
+    }
+    if (!isValidPassword($password)) {
+        header('location: ../view/sign-up.php?err=passwordInvalid');
+        exit();
+    }
+    if ($password !== $cpassword) {
+        header('location: ../view/sign-up.php?err=passwordMismatch');
+        exit();
+    }
+    if (!isValidPhone($phone, $country)) {
+        header('location: ../view/sign-up.php?err=firstNameEmpty');
+        exit();
+    }
+    if (!isValidBloodGroup($bloodGroup)) {
+        header('location: ../view/sign-up.php?err=invalidBloodGroup');
         exit();
     }
 
-    addUser($firstName, $lastName, $email, $password, $bloodGroup, $dob, $gender, $country, $phone, $role);
-    header('Location: ../view/login.php');
 
-} else header('Location: ../view/signup.php');
+    addUser($firstName, $lastName, $email, $password, $bloodGroup, $dob, $gender, $country, $phone, $role);
+    header('Location: ../view/sign-in.php');
+
+} else
+    header('Location: ../view/sign-up.php');
+
+// validation functions
+function isValidEmail($email)
+{
+    $atIndex = strpos($email, '@');
+    $dotIndex = strpos($email, '.');
+
+    if ($atIndex === false || $dotIndex === false) {
+        return false;
+    }
+    if ($atIndex === 0 || $dotIndex === (strlen($email) - 1)) {
+        return false;
+    }
+
+    if (strpos($email, '..') !== false) {
+        return false;
+    }
+
+    if (strpos($email, '.@') !== false) {
+        return false;
+    }
+
+    if (strpos($email, '@.') !== false) {
+        return false;
+    }
+
+    if (strpos($email, ' ') !== false) {
+        return false;
+    }
+
+    return true;
+}
+
+function isValidName($name) // "noman" = 5 // 0 1 2 3 4 name[3]
+{
+
+    if (strlen($name) < 3)
+        return false;
+
+    for ($i = 0; $i < strlen($name); $i++) { //nom@n a
+        $character = $name[$i];
+
+        if (!ctype_alpha($character) && $character !== ' ' && $character !== '.') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isValidBloodGroup($group)
+{
+    $available = array('A+', 'A-', 'B+', 'O+', 'AB+');
+    if (array_search($group, $available) === false) {
+        return false;
+    }
+    return true;
+
+}
+
+function isValidPassword($password)
+{
+    if (strlen($password) < 8)
+        return false;
+    return true;
+}
+
+function isValidPhone($phone, $region)
+{
+    $regions = array(
+        'India' => 5,
+        'United Kingdom' => 11,
+        'Bangladesh' => 11,
+        'US' => 11,
+        'Other' => 11
+    );
+
+    if (strlen($phone) != $regions[$region])
+        return false;
+
+    $allowedCharacters = '0123456789';
+
+    for ($i = 0; $i < strlen($phone); $i++) {
+        $character = $phone[$i];
+
+        if (strpos($allowedCharacters, $character) === false) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 ?>
-
-
- 
