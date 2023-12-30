08da95e8b3a336c07e557905f55491e3fe377e43
Tanvir Hasan Tamal <tanvirh103@gmail.com>
2023-12-31 00:32:34 +0600

    .

--- model/user-info-model.php
+++ model/user-info-model.php
@@ -76,7 +76,7 @@ require_once('database.php');
 
     function searchuser($name){
         $conn=dbConnection();
-        $sql="SELECT * FROM userinfo WHERE FirstName OR LastName LIKE'%$name%'";
+        $sql="SELECT * FROM userinfo WHERE CONCAT(FirstName,LastName) LIKE'%$name%'";
         $result=mysqli_query($conn,$sql);
         return $result;
     }
